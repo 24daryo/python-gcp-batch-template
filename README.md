ff59c04783a1921c1410cb300395fedad1fffa85
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-02-09 14:38:04 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# python-gcp-batch-template
-Python + GCP(Storage, SQL, Batch, Build)
+# python-cloudrun-test
+
+## 初期化
+
+```
+python -m vnev venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## デプロイ
+
+### そのままデプロイ
+
+コマンドラインで設定したいときに利用
+
+```
+gcloud run deploy --source .
+```
+
+### Cloud Buildでデプロイ
+
+```
+gcloud builds submit --config=cloudbuild.yaml --region=asia-northeast1
+```
---
+++ create-github-deploy-trigger.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ./.env
+
+# prodにpushしたらデプロイするトリガーを作成
+# branch-patternを指定しているの、そのファイルにpushしてデプロイ
+# or --tag-pattern=TAG_PATTERN
+gcloud beta builds triggers create github \
+    --repo-name=$CBT_REPO_NAME \
+    --repo-owner=$CBT_REPO_OWNER \
+    --branch-pattern=$CBT_BRANCH_PATTERN \
+    --build-config=$CBT_BUILD_CONFIG_FILE \
+    --include-logs-with-status \
+    --name=$CBT_TRIGGER_NAME \
+    --region=$CR_REGION \
+    --substitutions _CB_SERVICE_NAME=$CR_SERVICE_NAME,_CR_REGION=$CR_REGION
