e9638c028f2a472c419a4db6a206e41c66c7362c
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-02-09 15:53:54 +0900

    update build code

--- create-github-deploy-trigger.sh
+++ create-github-deploy-trigger.sh
@@ -4,13 +4,14 @@ source ./.env
 # prodにpushしたらデプロイするトリガーを作成
 # branch-patternを指定しているの、そのファイルにpushしてデプロイ
 # or --tag-pattern=TAG_PATTERN
+# キーの詳細：https://cloud.google.com/sdk/gcloud/reference/beta/builds/triggers/create/github
 gcloud beta builds triggers create github \
+    --name=$CBT_TRIGGER_NAME \
+    --description=$CBT_TRIGGER_DESCRIPTION \
+    --region=$CBT_REGION \
     --repo-name=$CBT_REPO_NAME \
     --repo-owner=$CBT_REPO_OWNER \
     --branch-pattern=$CBT_BRANCH_PATTERN \
     --build-config=$CBT_BUILD_CONFIG_FILE \
     --include-logs-with-status \
-    --require-approval \
-    --name=$CBT_TRIGGER_NAME \
-    --region=$CR_REGION \
-    --substitutions _CB_SERVICE_NAME=$CR_SERVICE_NAME,_CR_REGION=$CR_REGION
+    --substitutions _CR_SERVICE_NAME=$CR_SERVICE_NAME,_CR_REGION=$CR_REGION
