4c1dbecb75c758b31e5f89d07fc2282150549ead
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-02-10 01:03:21 +0900

    init

--- configs.py
+++ configs.py
@@ -25,7 +25,6 @@ else:
     # DB_URI = f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?unix_socket={INSTANCE_UNIX_SOCKET}/.s.PGSQL.5432"
     # DB_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?unix_socket={INSTANCE_UNIX_SOCKET}/.s.PGSQL.5432"
     # DB_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{INSTANCE_UNIX_SOCKET}/.s.PGSQL.5432/{DB_NAME}"
-    # DB_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={INSTANCE_UNIX_SOCKET}/.s.PGSQL.5432"
     DB_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={INSTANCE_UNIX_SOCKET}"
     # DB_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={INSTANCE_UNIX_SOCKET}"
     print(DB_URI)
