12bcdd74688b79fc15115fd8b9fa2200addc587d
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-02-09 17:50:26 +0900

    reset

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv(verbose=True)
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-   name = os.environ.get("NAME", "World")
-   return "Hello {}!".format(name)
+   name = os.environ.get("TEST_VARIABLE", "Failed")
+   return f"Hello World!, Read {name}"
 
 if __name__ == "__main__":
    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
