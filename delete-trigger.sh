70eef5371ce0806763b3d731c3cba8493da44ef6
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-02-13 23:13:56 +0900

    update delete trigger

--- delete-trigger.sh
+++ delete-trigger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./.env
 gcloud beta builds triggers delete $CBT_TRIGGER_NAME --region=$CBT_REGION
-gcloud beta scheduler jobs delete $CS_JOB_NAME --location=$CS_REGION
+gcloud beta scheduler jobs delete $CS_JOB_NAME --location=$CS_LOCATION
